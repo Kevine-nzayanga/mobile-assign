973bd34c5ac2ab4a9165b329d9b418d77b71b43b
Kevine-Nzayanga <124130681+Kevine-nzayanga@users.noreply.github.com>
2023-02-08 10:44:19 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import java.lang.IllegalArgumentException
 
 fun main(arg: Array<String>) {
     var fullname= arrayOf("kevine", "nzayanga", "miana")
-    var age: Byte = 50
+    var age: Short = 50
     var phonenumber = 756937596
     var weightinkg: Short= 234
     val kenyan: Boolean= true
